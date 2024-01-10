ad6ce918225f1e093b5a004a6f194d56305c9b59
harshani <ruwanthikapemarathna@gmail.com>
2024-01-10 15:15:09 +0530

    added mapstruct

---
+++ src/main/java/com/devstack/employeemanage/util/mapper/EmployeeMapper.java
@@ -0,0 +1,17 @@
+package com.devstack.employeemanage.util.mapper;
+
+import com.devstack.employeemanage.dto.request.RequestEmployeeDto;
+import com.devstack.employeemanage.dto.response.ResponseEmployeeDto;
+import com.devstack.employeemanage.entity.Employee;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
+    ResponseEmployeeDto toResponseEmployeeDto(Employee employee);
+    Employee toEmployee(RequestEmployeeDto dto);
+    List<ResponseEmployeeDto> toResponseEmployeeDtoList(List<Employee> employeeList);
+}
+
